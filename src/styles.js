ae876e88dea8fa83109f57b983a4477ca3f59287
Pedro Neves <pedro.hfn7@gmail.com>
2019-05-01 17:40:27 -0300

    feat: Adding helper styles

--- src/styles.js
+++ src/styles.js
@@ -2,6 +2,13 @@ import Colors from './colors';
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+	mt5: { marginLeft: 5 },
+	mt10: { marginLeft: 10 },
+	ml5: { marginLeft: 5 },
+	mr5: { marginRight: 5 },
+	ml10: { marginLeft: 10 },
+	mr10: { marginRight: 10 },
+	mb10: { marginBottom: 10 },
 	mb20: { marginBottom: 20 },
 	touch: {
 		padding: 20,
@@ -17,14 +24,24 @@ export default StyleSheet.create({
 	touchWhite: {
 		backgroundColor: Colors.white
 	},
+	touchGreen: {
+		backgroundColor: Colors.green
+	},
+	touchRed: {
+		backgroundColor: Colors.red
+	},
+	touchBrightRed: {
+		backgroundColor: Colors.brightRed
+	},
 	touchText: {
 		fontSize: 20,
+		textAlign: 'center'
 	},
 	touchTextBlack: {
-		color: Colors.black,
+		color: Colors.black
 	},
 	touchTextWhite: {
-		color: Colors.white,
+		color: Colors.white
 	},
 	textInputContainer: {
 		padding: 5,
@@ -34,6 +51,15 @@ export default StyleSheet.create({
 	},
 	textInput: {
 		fontSize: 25
+	},
+	textBig: {
+		fontSize: 40
+	},
+	textMedium: {
+		fontSize: 30
+	},
+	textSmall: {
+		fontSize: 15
 	},
 	containerMiddle: {
 		flex: 1,
