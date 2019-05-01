618a91b8761d7095267c33936e96cd9fa01a66d0
Pedro Neves <pedro.hfn7@gmail.com>
2019-05-01 12:46:34 -0300

    feat: Adding save deck actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
 export const LOAD_DECKS_START = 'LOAD_DECKS_START';
 export const LOAD_DECKS_SUCCESS = 'LOAD_DECKS_SUCCESS';
 export const LOAD_DECKS_FAIL = 'LOAD_DECKS_FAIL';
+export const SAVE_DECK_START = 'SAVE_DECK_START';
+export const SAVE_DECK_SUCCESS = 'SAVE_DECK_SUCCESS';
+export const SAVE_DECK_FAIL = 'SAVE_DECK_FAIL';
 export const SET_DECKS = 'SET_DECKS';
 
 export function startLoadingDecks () {
@@ -14,6 +17,21 @@ export function successfullyLoadedDecks () {
 export function failedLoadedDecks (error) {
 	return {
 		type: LOAD_DECKS_FAIL,
+		error
+	}
+}
+
+export function startSavingDeck () {
+	return { type: SAVE_DECK_START }
+}
+
+export function successfullySavedDeck () {
+	return { type: SAVE_DECK_SUCCESS }
+}
+
+export function failedSaveDeck (error) {
+	return {
+		type: SAVE_DECK_FAIL,
 		error
 	}
 }
