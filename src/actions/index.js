61bb161c7e69ac224ba937202dd06509ce573917
Pedro Neves <pedro.hfn7@gmail.com>
2019-05-01 14:37:58 -0300

    feat: Adding actions for card saving

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,9 @@ export const LOAD_DECKS_FAIL = 'LOAD_DECKS_FAIL';
 export const SAVE_DECK_START = 'SAVE_DECK_START';
 export const SAVE_DECK_SUCCESS = 'SAVE_DECK_SUCCESS';
 export const SAVE_DECK_FAIL = 'SAVE_DECK_FAIL';
+export const SAVE_CARD_START = 'SAVE_CARD_START';
+export const SAVE_CARD_SUCCESS = 'SAVE_CARD_SUCCESS';
+export const SAVE_CARD_FAIL = 'SAVE_CARD_FAIL';
 export const SET_DECKS = 'SET_DECKS';
 
 export function startLoadingDecks () {
@@ -32,6 +35,21 @@ export function successfullySavedDeck () {
 export function failedSaveDeck (error) {
 	return {
 		type: SAVE_DECK_FAIL,
+		error
+	}
+}
+
+export function startSavingCard () {
+	return { type: SAVE_CARD_START }
+}
+
+export function successfullySavedCard () {
+	return { type: SAVE_CARD_SUCCESS }
+}
+
+export function failedSaveCard (error) {
+	return {
+		type: SAVE_CARD_FAIL,
 		error
 	}
 }
